f542a0709dfc95cbc41d4145ddd7597683e025ee
Tj <tjvilliard@gmail.com>
2022-11-08 13:03:50 -0700

    Basic data loading in script

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* DATA LOADING */
+async function loadData () {
+
+    const dataset_array = []
+    for (let i = 13; i < 22; i++) {
+        path = `data/TeamStats/ cfb${i}.csv`
+        let dataset = await d3.csv(path);
+        dataset_array.push(dataset);
+      }
+
+    console.log(dataset_array)
+    return dataset_array;
+  }
+  
+  loadData().then((loadedData) => {
+
+    console.log(loadedData)
+
+    // globalApplicationState.wordData = loadedData;
+    // globalApplicationState.tableData = loadedData;
+
+    // globalApplicationState.beeswarm = new Beeswarm(globalApplicationState);
+    // globalApplicationState.table = new Table(globalApplicationState);
+    
+
+  });
+  const globalApplicationState = {
+    tableData: null,
+    wordData: null,
+    separated: false,
+    beeswarm: null,
+    table: null,
+  };
