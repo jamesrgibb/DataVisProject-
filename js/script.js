a6f8b001f3462d9e356507aeeba317e70a30a463
Tj <tjvilliard@gmail.com>
2022-11-10 10:56:53 -0700

    GroupByTeam works. Mpas from team to all teamdata

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ async function loadData () {
 
     const dataset_array = []
     for (let i = 13; i < 22; i++) {
-        path = `data/TeamStats/ cfb${i}.csv`
+        path = `data/TeamStats/cfb${i}.csv`
         let dataset = await d3.csv(path);
         dataset_array.push(dataset);
       }
@@ -16,18 +16,63 @@ async function loadData () {
 
     console.log(loadedData)
 
-    // globalApplicationState.wordData = loadedData;
-    // globalApplicationState.tableData = loadedData;
+    globalApplicationState.data = loadedData
 
-    // globalApplicationState.beeswarm = new Beeswarm(globalApplicationState);
-    // globalApplicationState.table = new Table(globalApplicationState);
+    // set default table state
+    const defaultState = {
+
+    }
+    
+    // ... then 
+    globalApplicationState.tableState = defaultState;
+
+    // initialize table
+    globalApplicationState.table = new Table(globalApplicationState)
+
+
+    let teamMap = groupByTeam(loadedData);
+
     
 
   });
-  const globalApplicationState = {
+
+
+  function groupByTeam(array){
+
+    
+    const teamMap = new Map();
+
+    for(let i =0; i < 9; i++){
+        let yearData = array[i]
+        let teamGrouping = d3.group(yearData, d=>d.Team);
+
+        // In this format, key is the value we want and value is the key
+        teamGrouping.forEach(function (value, key) {
+            // check if map has key then append teamdata to map 
+            if(teamMap.has(key)){
+                let teamdata = teamMap.get(key)
+                teamdata.push(value)
+                teamMap.set(key, teamdata)
+            } else {
+                teamMap.set(key, [value])
+            }
+        });
+    }
+
+    return teamMap
+  }
+
+  const tableState = {
+    drawData: null, 
     tableData: null,
-    wordData: null,
-    separated: false,
-    beeswarm: null,
+    seasonalData: null,
+    defaultTableData: null, 
+  }
+
+  const globalApplicationState = {
+    tableState: null, 
+    data: null,
+    histogram: null,
     table: null,
+    correlation: null
   };
--- js/table.js
+++ js/table.js
@@ -1,5 +1,20 @@
 
 class Table{
 
+    constructor(globalState){
+
+        // determines which season and values are drawn by the table 
+        this.state = globalState.tableState;
+
+        this.teams = Array.from(d3.group)
+    }
+
+
+    /** Called to update the columns when switching out table data  */
+    setColumns (array) {
+        let headers = d3.select("#columnHeaders");
+        headers.selectAll('td').data(array).join('td').text(d=>d)
     
+      }
+
 }
