be895eb1db8e2d1a6271bbb81475d6d87f50eeef
Tj <tjvilliard@gmail.com>
2022-11-10 11:12:25 -0700

    added to html structure. Changed deafualt and tablestate in script.

--- js/script.js
+++ js/script.js
@@ -9,8 +9,32 @@ async function loadData () {
       }
 
     console.log(dataset_array)
+    // returns 9 len array, each element is full dataset from one season
     return dataset_array;
   }
+
+
+  const tableState = {
+    // data the table is drawing, could be filtered
+    drawData: null, 
+    // unfiltered current season dataset
+    tableData: null,
+    
+    // unsure if these two are neccessary at the moment 
+    // seasonalData: null,
+    //defaultTableData: null, 
+  }
+
+  const globalApplicationState = {
+    // current state of the table 
+    tableState: null, 
+    // full set of 9 seasons of data 
+    data: null, 
+    teamMap: null,
+    histogram: null,
+    table: null,
+    correlation: null
+  };
   
   loadData().then((loadedData) => {
 
@@ -18,8 +42,15 @@ async function loadData () {
 
     globalApplicationState.data = loadedData
 
+    // get team data across all 9 seasons into map 
+    globalApplicationState.teamMap = groupByTeam(loadedData);
+
     // set default table state
     const defaultState = {
+        drawData: null, 
+        tableData: null,
+        // seasonalData: null,
+        defaultTableData: null, 
 
     }
     
@@ -29,8 +60,7 @@ async function loadData () {
     // initialize table
     globalApplicationState.table = new Table(globalApplicationState)
 
-
-    let teamMap = groupByTeam(loadedData);
+    
 
     
 
@@ -62,17 +92,3 @@ async function loadData () {
     return teamMap
   }
 
-  const tableState = {
-    drawData: null, 
-    tableData: null,
-    seasonalData: null,
-    defaultTableData: null, 
-  }
-
-  const globalApplicationState = {
-    tableState: null, 
-    data: null,
-    histogram: null,
-    table: null,
-    correlation: null
-  };
