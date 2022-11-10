b7cef89c929cb24df03df8fb9ff2e2c605c0e3ff
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2022-11-10 09:32:23 -0700

    figured out the group by team and year

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
-async function loadData () {
+const dataset_array = []
+let teamKeys;
+let teams;
+let teamData = {};
+for (let i = 13; i < 22; i++) {
+    path = `data/TeamStats/cfb${i}.csv`
+    let dataset = d3.csv(path).then(function (data) {
+        teams = d3.groups(data, d => d.Team)
+        teamKeys = teams.map((t, i) => {
+            // console.log(t[0])
+            // console.log(i)
+            return {key: t[0], pos: i};
+        })
+        // console.log(teamKeys)
 
-    const dataset_array = []
-    for (let i = 13; i < 22; i++) {
-        path = `data/TeamStats/ cfb${i}.csv`
-        let dataset = await d3.csv(path);
-        dataset_array.push(dataset);
-    }
-
-    console.log(dataset_array)
-    return dataset_array;
+        teamKeys.forEach(team => {
+            teamData[team.key] = data.filter(
+                d => d.Team === team.key)
+        })
+    })
+    dataset_array.push(teamData)
+    // console.log(Object.keys(dataset_array))
 }
 
-loadData().then((loadedData) => {
+t = new Table(dataset_array)
 
-    console.log(loadedData)
-
-    // globalApplicationState.wordData = loadedData;
-    // globalApplicationState.tableData = loadedData;
-
-    // globalApplicationState.beeswarm = new Beeswarm(globalApplicationState);
-    // globalApplicationState.table = new Table(globalApplicationState);
-
-
-});
+// loadData().then((loadedData) => {
+//
+//
+//     // globalApplicationState.wordData = loadedData;
+//     // globalApplicationState.tableData = loadedData;
+//
+//     // globalApplicationState.beeswarm = new Beeswarm(globalApplicationState);
+//     // globalApplicationState.table = new Table(globalApplicationState);
+//
+//
+// });
 const globalApplicationState = {
     tableData: null,
     wordData: null,
--- js/table.js
+++ js/table.js
@@ -1,4 +1,32 @@
 class Table{
 
-    
+    constructor(data){
+        this.data = data;
+        this.columns = [];
+        this.rows = [];
+        this.columnWidths = [];
+        this.rowHeights = [];
+        this.columnMaxWidths = [];
+        this.rowMaxHeights = [];
+       this.headerData = [
+           {
+               sorted: false,
+               ascending: false,
+               key: 'rank'
+           },
+           {
+               sorted: false,
+               ascending: false,
+               key: 'Win',
+               alterFunc: d => Math.abs(+d)
+           },
+           {
+               sorted: false,
+               ascending: false,
+               key: 'Loss',
+               alterFunc: d => +d
+           },
+
+       ]
+    }
 }
